82157f2c2bf219c4b6ce617471dcf270c4ed2845
Luca Gioppo <gioppoluca@libero.it>
2024-06-09 16:41:01 +0200

    feature stays in the same folder

--- clone_micado_development.sh
+++ clone_micado_development.sh
@@ -3,11 +3,8 @@
 # URL of the micado_development repository
 micado_development_repo="https://github.com/micado-eu/micado_development"
 
-# Clone the micado_development repository
-git clone "$micado_development_repo"
-
-# Change to the micado_development directory
-cd micado_development
+# Clone the micado_development repository into the current directory
+git clone "$micado_development_repo" .
 
 # Make setup_micado_repos.sh executable
 chmod +x setup_micado_repos.sh
