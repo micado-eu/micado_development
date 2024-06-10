542d96f209a7d738ce3bdc0f765fd18c149fa61f
Luca Gioppo <gioppoluca@libero.it>
2024-06-10 15:24:55 +0200

    feature: added setting for hosts file

--- setup_micado_repos.sh
+++ setup_micado_repos.sh
@@ -31,3 +31,30 @@ for dir in "${additional_dirs[@]}"; do
 done
 
 echo "All repositories have been cloned and additional directories created in micado_development."
+
+# Define the entries to be added to /etc/hosts
+HOSTS_ENTRIES=$(cat <<EOF
+# Micado Development Hostnames
+127.0.0.1 migrant.micado.local
+127.0.0.1 micado-pa.micado.local
+127.0.0.1 ngo.micado.local
+127.0.0.1 api.micado.local
+127.0.0.1 gateway.micado.local
+127.0.0.1 dashboard.micado.local
+127.0.0.1 admin.micado.local
+127.0.0.1 identity.micado.local
+127.0.0.1 monitoring.micado.local
+127.0.0.1 translate.micado.local
+127.0.0.1 git.micado.local
+127.0.0.1 portainer.micado.local
+127.0.0.1 admin2.micado.local
+127.0.0.1 multichatbot
+127.0.0.1 traefik.micado.local
+EOF
+)
+
+# Append the entries to /etc/hosts
+echo "$HOSTS_ENTRIES" | sudo tee -a /etc/hosts > /dev/null
+
+echo "Entries added to /etc/hosts successfully."
+
